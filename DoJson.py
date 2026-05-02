0e11b7d536c1ea8cfb2eddcf7ddfd029cc10d649
hybolic <hybolic3@hotmail.com>
2026-05-02 21:40:19 +1000

    Fish and Biomes

--- DoJson.py
+++ DoJson.py
@@ -1,29 +1,34 @@
 from PRELOADER import active_venv
+from json      import dumps, loads
+from os.path   import exists
+from os        import remove
 active_venv()
-import os, json
 
 
 OUTPUT_DIR = "./database/Auras/"
-INPUT_DIR  = "./pre_database/"
-AuraList_FILE = "Auras"
+INPUT_DIR  = "./pre_database/Auras/"
+AuraList_FILE = "AllAuras"
 ENCODER = "UTF-8"
 
         
 def WriteDictToFile(FILE, OBJECT, INDENT="    ",SEPERATOR=(', ', ' : '), ENCODER=ENCODER):
-    if os.path.exists(FILE):
-        os.remove(FILE)
+    if exists(FILE):
+        remove(FILE)
     with open(FILE,"w",encoding=ENCODER) as file:
-        file.write(json.dumps(OBJECT, ensure_ascii=False, indent=INDENT,separators=SEPERATOR))
-
-#TODO: Crawl Pre_database folder and append to correct database folder
-
-file = AuraList_FILE
-OUTPUT_FILE = file
-with open(INPUT_DIR + file + ".json", "r", encoding="UTF-8") as file:
-    AuraList:dict = json.loads(file.read())
-    
-    WriteDictToFile(OUTPUT_DIR + OUTPUT_FILE + ".json", AuraList)
-    WriteDictToFile(OUTPUT_DIR + OUTPUT_FILE + ".mini.json", AuraList, INDENT=None,SEPERATOR=(",",":"))
+        file.write(dumps(OBJECT, ensure_ascii=False, indent=INDENT,separators=SEPERATOR))
+        
+def QuickTransfer(OUTPUT_DIR, INPUT_DIR, AuraList_FILE):
+    with open(INPUT_DIR + AuraList_FILE + ".json", "r", encoding="UTF-8") as file:
+        AuraList:dict = loads(file.read())
+        
+        WriteDictToFile(OUTPUT_DIR + AuraList_FILE + ".json", AuraList)
+        WriteDictToFile(OUTPUT_DIR + AuraList_FILE + ".mini.json", AuraList, INDENT=None,SEPERATOR=(",",":"))
+        
+#DO AURA STUFF
+with open(INPUT_DIR + AuraList_FILE + ".json", "r", encoding="UTF-8") as file:
+    AuraList:dict = loads(file.read())
+    WriteDictToFile(OUTPUT_DIR + AuraList_FILE + ".json", AuraList)
+    WriteDictToFile(OUTPUT_DIR + AuraList_FILE + ".mini.json", AuraList, INDENT=None,SEPERATOR=(",",":"))
     
     compacted = AuraList.copy()
     for key, value in compacted.items():
@@ -31,12 +36,17 @@ with open(INPUT_DIR + file + ".json", "r", encoding="UTF-8") as file:
         if "Title" in value.items():
             value.pop("Title")
             
-    WriteDictToFile(OUTPUT_DIR + OUTPUT_FILE + ".compact.json", compacted)
-    WriteDictToFile(OUTPUT_DIR + OUTPUT_FILE + ".compact.mini.json", compacted, INDENT=None,SEPERATOR=(",",":"))
+    WriteDictToFile(OUTPUT_DIR + AuraList_FILE + ".compact.json", compacted)
+    WriteDictToFile(OUTPUT_DIR + AuraList_FILE + ".compact.mini.json", compacted, INDENT=None,SEPERATOR=(",",":"))
     
     obtainable = AuraList.copy()
     for key, value in AuraList.items():
         if "Unobtainable" in value["rarity"] and not ("native" in value.keys()):
             obtainable.pop(key)
-    WriteDictToFile(OUTPUT_DIR + OUTPUT_FILE + ".obtainable.json", obtainable)
-    WriteDictToFile(OUTPUT_DIR + OUTPUT_FILE + ".obtainable.mini.json", obtainable, INDENT=None,SEPERATOR=(",",":"))
+    WriteDictToFile(OUTPUT_DIR + AuraList_FILE + ".obtainable.json", obtainable)
+    WriteDictToFile(OUTPUT_DIR + AuraList_FILE + ".obtainable.mini.json", obtainable, INDENT=None,SEPERATOR=(",",":"))
+
+QuickTransfer("./database/Biome/", "./pre_database/Biome/", "Biome")
+QuickTransfer("./database/Fish/",  "./pre_database/Fish/",  "Fish")
+QuickTransfer("./database/Fish/",  "./pre_database/Fish/",  "Mutation")
+QuickTransfer("./database/Fish/",  "./pre_database/Fish/",  "Rarity")
