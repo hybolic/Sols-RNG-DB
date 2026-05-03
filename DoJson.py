c4ddf5948cde011b68dcce065adac4809ec15b85
hybolic <hybolic3@hotmail.com>
2026-05-03 12:59:38 +1000

    Aura Ability+Achievements

--- DoJson.py
+++ DoJson.py
@@ -11,42 +11,56 @@ AuraList_FILE = "AllAuras"
 ENCODER = "UTF-8"
 
         
-def WriteDictToFile(FILE, OBJECT, INDENT="    ",SEPERATOR=(', ', ' : '), ENCODER=ENCODER):
+def WriteDictToFile(FILE, OBJECT, INDENT="    ",SEPERATOR=(', ', ' : '), ENCODER=ENCODER, PRSEP=None):
     if exists(FILE):
         remove(FILE)
+    if(PRSEP != None):
+        print(SEP + FILE)
     with open(FILE,"w",encoding=ENCODER) as file:
         file.write(dumps(OBJECT, ensure_ascii=False, indent=INDENT,separators=SEPERATOR))
         
 def QuickTransfer(OUTPUT_DIR, INPUT_DIR, AuraList_FILE):
+    print(INPUT_DIR + AuraList_FILE + ".json > " + OUTPUT_DIR + AuraList_FILE + ".json")
+    print(INPUT_DIR + AuraList_FILE + ".json > " + OUTPUT_DIR + AuraList_FILE + ".mini.json")
     with open(INPUT_DIR + AuraList_FILE + ".json", "r", encoding="UTF-8") as file:
         AuraList:dict = loads(file.read())
         
         WriteDictToFile(OUTPUT_DIR + AuraList_FILE + ".json", AuraList)
         WriteDictToFile(OUTPUT_DIR + AuraList_FILE + ".mini.json", AuraList, INDENT=None,SEPERATOR=(",",":"))
+    print()
         
 #DO AURA STUFF
+print(INPUT_DIR + AuraList_FILE + ".json |           OUT FILES")
+SEP = ""
+for i in range(len(INPUT_DIR + AuraList_FILE + ".json > ") - 3):
+    SEP+=" "
+SEP += " > "
 with open(INPUT_DIR + AuraList_FILE + ".json", "r", encoding="UTF-8") as file:
     AuraList:dict = loads(file.read())
-    WriteDictToFile(OUTPUT_DIR + AuraList_FILE + ".json", AuraList)
-    WriteDictToFile(OUTPUT_DIR + AuraList_FILE + ".mini.json", AuraList, INDENT=None,SEPERATOR=(",",":"))
+    WriteDictToFile(OUTPUT_DIR + AuraList_FILE + ".json", AuraList,PRSEP=SEP)
+    WriteDictToFile(OUTPUT_DIR + AuraList_FILE + ".mini.json", AuraList, INDENT=None,SEPERATOR=(",",":"),PRSEP=SEP)
     
     compacted = AuraList.copy()
     for key, value in compacted.items():
-        value.pop("fandom_wiki")
-        if "Title" in value.items():
+        if "fandom_wiki" in value.keys():
+            value.pop("fandom_wiki")
+        if "Title" in value.keys():
             value.pop("Title")
+        if "Ability" in value.keys():
+            value.pop("Ability")
             
-    WriteDictToFile(OUTPUT_DIR + AuraList_FILE + ".compact.json", compacted)
-    WriteDictToFile(OUTPUT_DIR + AuraList_FILE + ".compact.mini.json", compacted, INDENT=None,SEPERATOR=(",",":"))
+    WriteDictToFile(OUTPUT_DIR + AuraList_FILE + ".compact.json", compacted,PRSEP=SEP)
+    WriteDictToFile(OUTPUT_DIR + AuraList_FILE + ".compact.mini.json", compacted, INDENT=None,SEPERATOR=(",",":"),PRSEP=SEP)
     
     obtainable = AuraList.copy()
     for key, value in AuraList.items():
         if "Unobtainable" in value["rarity"] and not ("native" in value.keys()):
             obtainable.pop(key)
     WriteDictToFile(OUTPUT_DIR + AuraList_FILE + ".obtainable.json", obtainable)
-    WriteDictToFile(OUTPUT_DIR + AuraList_FILE + ".obtainable.mini.json", obtainable, INDENT=None,SEPERATOR=(",",":"))
-
+    WriteDictToFile(OUTPUT_DIR + AuraList_FILE + ".obtainable.mini.json", obtainable, INDENT=None,SEPERATOR=(",",":"),PRSEP=SEP)
+print()
 QuickTransfer("./database/Biome/", "./pre_database/Biome/", "Biome")
 QuickTransfer("./database/Fish/",  "./pre_database/Fish/",  "Fish")
 QuickTransfer("./database/Fish/",  "./pre_database/Fish/",  "Mutation")
 QuickTransfer("./database/Fish/",  "./pre_database/Fish/",  "Rarity")
+QuickTransfer("./database/Achievements/",  "./pre_database/Achievements/",  "Achievements")
